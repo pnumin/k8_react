832ea1e6106bae98a790f0f7aa83178041b38c39
pnumin <pnumin@pusan.ac.kr>
2024-09-23 15:18:49 +0900

    app layout

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
-import Hello from './01/Hello' ;
+import { FaHome } from "react-icons/fa";
+
+// import Hello from './01/Hello' ;
+// import MyClock from './02/MyClock' ;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Hello />
+    <div className="w-full xl:w-10/12 h-screen mx-auto
+                    flex flex-col justify-center items-center">
+      <header className='w-full h-20
+                         flex justify-between items-center
+                         bg-slate-200'>
+        <p className='text-2xl font-bold p-5'>
+          K-digital 8기
+        </p>
+        <p className='text-4xl font-bold p-5'>
+          <FaHome />
+        </p>
       </header>
+      <main className='w-full grow
+                       flex flex-col justify-center items-center
+                       overflow-y-scroll'>
+
+      </main>
+      <footer className='w-full h-20
+                         flex justify-center items-center
+                         bg-black text-white'>
+        <p>K-digital 8기 김경민</p>                  
+      </footer>
     </div>
   );
 }
