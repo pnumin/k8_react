885f69fe840e49c47336eca0cde35b4684fd162a
pnumin <pnumin@pusan.ac.kr>
2024-09-24 15:28:07 +0900

    probs예제

--- src/04/MyListItem.js
+++ src/04/MyListItem.js
@@ -1,25 +1,26 @@
 
-export default function MyListItem() {
+export default function MyListItem({imgUrl, content, title}) {
   return (
-    <div className='w-full 
+    <div className='w-full h-full
                       border border-gray-300
                       flex justify-center items-center'>
-      <div className='w-1/3 flex justify-center items-start'>
-        이미지
+      <div className='w-1/3 flex justify-start items-start'>
+        <img src={imgUrl} alt={title} />
       </div>
-      <div className='w-2/3 flex flex-col justify-between items-center'>
-        <div>
-          <div>
-            title
+      <div className='w-2/3 h-full p-5 flex flex-col justify-start items-center'>
+        <div className="flex flex-col  h-3/4">
+          <div className="text-2xl font-bold mb-2">
+            {title}
           </div>
-          <div>
-            content
+          <div className="text-sm">
+            {content}
           </div>
         </div>
-        <div>
-          <span>❤️</span>
-          <span>좋아요</span>
-          <span>0</span>
+        <div className="flex w-full h-1/4
+                        justify-end items-end">
+          <div className="text-2xl cursor-pointer">❤️</div>
+          <div className="mx-2 font-bold">좋아요</div>
+          <div>0</div> 
         </div>
       </div>
     </div>
