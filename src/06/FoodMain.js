ac3d5b84b85cf3512dd99a5671590090420dc9c3
pnumin <pnumin@pusan.ac.kr>
2024-09-27 15:48:00 +0900

    foodbank

--- src/06/FoodMain.js
+++ src/06/FoodMain.js
@@ -1,11 +1,19 @@
 import fooddata from './fooddata.json' ;
 import FoodCard from './FoodCard';
+import { useState, useEffect } from 'react';
+
 export default function FoodMain() {
-  console.log(fooddata)
+  const [tags, setTags] = useState() ;
+
+
   return (
-    <div>
-      FoodMain
-      <FoodCard />
+    <div className='w-full flex flex-col justify-start h-screen'>
+      <div className='w-full h-20 bg-blue-100'>
+        {/* {bts} */}
+      </div>
+      <div className='w-full overflow-auto grid grid-cols-1 xl:grid-cols-2 gap-4 p-4'>
+        {tags}
+      </div>
     </div>
   )
 }
