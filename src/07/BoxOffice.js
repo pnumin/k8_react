9791bbb91c5287017e3b0538d2d7b0c168ec2dd0
pnumin <pnumin@pusan.ac.kr>
2024-09-30 10:38:04 +0900

    박스오피스

--- src/07/BoxOffice.js
+++ src/07/BoxOffice.js
@@ -1,8 +1,18 @@
-
+import { useEffect, useState } from "react" ; 
 export default function BoxOffice() {
+
+  //맨처음 한번 실행
+  useEffect(()=>{
+    const apiKey = process.env.REACT_APP_MV_KEY ;
+    console.log('apiKey=', apiKey) ;
+  }, []);
+ 
+
   return (
-    <div>
-      BoxOffice
+    <div className="h-screen flex flex-col justify-center items-center">
+      <div>
+       
+      </div>
     </div>
   )
 }
