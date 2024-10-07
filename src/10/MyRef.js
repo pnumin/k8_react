e5f45b1255fc34fae5ab3d4ba3500ab5182c30f1
pnumin <pnumin@pusan.ac.kr>
2024-10-07 17:45:28 +0900

    ref변수

--- src/10/MyRef.js
+++ src/10/MyRef.js
@@ -1,25 +1,41 @@
 import TailButton from "../UI/TailButton" ;
+import { useState, useRef } from "react";
 export default function MyRef() {
+  //state변수 
+  const [valS, setValS] = useState(0);
+
+  //ref변수 
+  const valR = useRef(0) ; 
+
+  //컴포넌트 변수
+  let valC = 0 ;
 
   const handleBClick = () =>{
-
+    valC = valC + 1 ;
+    console.log('valC = ', valC) ;
   }
   const handleSClick = () =>{
-    
+    setValS(valS + 1) ;
   }
   const handleRClick = () =>{
-    
+    valR.current = valR.current + 1 ; 
+    console.log('valR=', valR);
+  }
+
+  const handleAdd = () => {
+
   }
   return (
-    <div className="w-3/5 grid grid-cols-3 gap-4 mt-10">
+    <>
+    <div className="w-3/5 grid grid-cols-3 gap-4 my-10 ">
       <div className="text-xl font-bold text-blue-800">
-        컴포넌트 변수 : 
+        컴포넌트 변수 : {valC}
       </div>
       <div className="text-xl font-bold text-orange-800">
-        State 변수 : 
+        State 변수 : {valS}
       </div>
       <div className="text-xl font-bold text-lime-800">
-        Ref 변수 : 
+        Ref 변수 : {valR.current}
       </div>
       <div>
           <TailButton caption = '컴포넌트 변수'
@@ -37,5 +53,25 @@ export default function MyRef() {
                         handleClick = {handleRClick} /> 
       </div>
     </div>
+    <div className="w-3/5 grid grid-cols-5 gap-2 p-2 bg-slate-200">
+      <div className="flex justify-center items-center text-center">
+        <input type='number' id='txt1' name='txt1' className="h-10 w-24" />
+      </div>
+      <div className="flex justify-center items-center text-2xl font-bold text-center">
+        +
+      </div>
+      <div className="flex justify-center items-center text-center">
+        <input type='number' id='txt2' name='txt2' className="h-10 w-24" />
+      </div>
+      <div className="flex justify-center items-center text-center">
+        <TailButton caption = '  =  '
+                      color = 'orange'
+                      handleClick = {handleAdd} />
+      </div>
+      <div className="flex justify-center items-center text-center">
+        <input type='number' id='txt3' name='txt3' readOnly className="h-10 w-24" />
+      </div>
+    </div>
+    </>
   )
 }
