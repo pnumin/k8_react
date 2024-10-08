74f433d2d5f8e9111fb0bcc006040229eaebb949
pnumin <pnumin@pusan.ac.kr>
2024-10-08 14:29:31 +0900

    Ref 변수

--- src/10/MyRef.js
+++ src/10/MyRef.js
@@ -1,11 +1,14 @@
 import TailButton from "../UI/TailButton" ;
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 export default function MyRef() {
   //state변수 
   const [valS, setValS] = useState(0);
 
   //ref변수 
-  const valR = useRef(0) ; 
+  const valR = useRef(0) ;
+  const x = useRef() ;
+  const y = useRef() ;
+  const z = useRef() ;
 
   //컴포넌트 변수
   let valC = 0 ;
@@ -23,8 +26,27 @@ export default function MyRef() {
   }
 
   const handleAdd = () => {
-
+    if (x.current.value == '') {
+      alert('값을 입력하세요.') ;
+      x.current.focus();
+      return;
+    }
+    if (y.current.value == '') {
+      alert('값을 입력하세요.') ;
+      y.current.focus();
+      return;
+    }
+    
+    z.current.value = parseInt(x.current.value) + parseInt(y.current.value) ;
   }
+
+  const handleFocus = () => {
+    z.current.value = '' ;
+  }
+  useEffect(()=>{
+    x.current.focus();  
+  }, []) ;
+
   return (
     <>
     <div className="w-3/5 grid grid-cols-3 gap-4 my-10 ">
@@ -55,13 +77,17 @@ export default function MyRef() {
     </div>
     <div className="w-3/5 grid grid-cols-5 gap-2 p-2 bg-slate-200">
       <div className="flex justify-center items-center text-center">
-        <input type='number' id='txt1' name='txt1' className="h-10 w-24" />
+        <input ref={x} type='number' id='txt1' name='txt1' 
+              className="text-center h-10 w-24" 
+              onFocus={handleFocus} />
       </div>
       <div className="flex justify-center items-center text-2xl font-bold text-center">
         +
       </div>
       <div className="flex justify-center items-center text-center">
-        <input type='number' id='txt2' name='txt2' className="h-10 w-24" />
+        <input ref={y} type='number' id='txt2' name='txt2' 
+                className="text-center h-10 w-24"
+                onFocus={handleFocus}  />
       </div>
       <div className="flex justify-center items-center text-center">
         <TailButton caption = '  =  '
@@ -69,7 +95,8 @@ export default function MyRef() {
                       handleClick = {handleAdd} />
       </div>
       <div className="flex justify-center items-center text-center">
-        <input type='number' id='txt3' name='txt3' readOnly className="h-10 w-24" />
+        <input ref={z} type='number' id='txt3' name='txt3' readOnly 
+            className="text-center h-10 w-24" />
       </div>
     </div>
     </>
