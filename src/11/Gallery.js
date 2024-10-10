7f6fb255994bd33cecd7821fe3fbe22771a185c8
pnumin <pnumin@pusan.ac.kr>
2024-10-10 09:25:30 +0900

    관광앱

--- src/11/Gallery.js
+++ src/11/Gallery.js
@@ -1,15 +1,31 @@
 import TailCard from "../UI/TailCard" ;
 import TailButton from "../UI/TailButton";
+import { useEffect, useRef } from "react";
 export default function Gallery() {
+
+  const x = useRef() ;
+
+  const handleCancel = () => {
+    x.current.value = '' ;
+    x.current.focus();
+  }
+
+  useEffect(()=>{
+    x.current.focus() ;
+  },[]);
+
+
   return (
     <div className="w-full flex justify-center">
       <div className="w-10/12 p-5">
         <h1 className="w-full flex justify-center text-3xl mb-5">
           한국관광공사 사진 정보
         </h1>
-        <div className="w-full p-5 bg-blue-50 grid grid-cols-1 lg:grid-cols-2 gap-2">
+        <div className="w-full p-5 bg-blue-50 
+                        grid grid-cols-1 lg:grid-cols-2 gap-2">
           <div className="flex justify-center  lg:justify-end items-center">
           <input type='text' className="w-10/12 form-input"
+                 ref = {x}
                  id='kw' name='kw' />
           </div>
           <div className="flex justify-center lg:justify-start items-center">
@@ -19,7 +35,7 @@ export default function Gallery() {
                       size = 'w-1/2'/>
           <TailButton caption = '취소'
                       color = 'blue'
-                      handleClick = ''
+                      handleClick = {handleCancel}
                       size = 'w-1/2' />
           </div>
         </div>  
