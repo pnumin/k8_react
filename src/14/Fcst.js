47ad44d4203525ca80b3a5dc83b209edf6652ee3
pnumin <pnumin@pusan.ac.kr>
2024-10-15 17:37:31 +0900

    일기예보

--- src/14/Fcst.js
+++ src/14/Fcst.js
@@ -1,26 +1,75 @@
-import TailButton from "../UI/TailButton" ;
+import TailButton from "../UI/TailButton";
+import getxy from './getxy.json';
+import { useNavigate } from "react-router-dom";
+import { useRef } from "react";
 
 export default function Fcst() {
+  // const sido = getxy.map(item => item["1단계"])
+  //                   .map(item => <option key={item} value={item}>
+  //                                 {item}                 
+  //                                 </option>) ;
+
+  // let sido = [] ;
+  // for (let i = 0 ; i < getxy.length ; i++) {
+  //   sido.push(getxy[i]["1단계"]) ;
+  // }
+
+  const sido = getxy.map(item => <option key={item["1단계"]}
+                                    value={item["1단계"]}>
+                                    {item["1단계"]}
+                                  </option>);
+  
+  const navigate = useNavigate() ;
+
+  //날짜
+  const txtDt = useRef();
+  //지역
+  const txtArea = useRef() ;
+
+  //버튼이 눌러졌을때
+  const handleOk = (gubun) => {
+    if (txtDt.current.value === '') {
+      alert('날짜를 선택하세요.')
+      txtDt.current.focus();
+      return;
+    }
+    if (txtArea.current.value === '') {
+      alert('지역을 선택하세요.')
+      txtArea.current.focus();
+      return;
+    }
+
+    console.log(gubun)
+    const dt = txtDt.current.value.replaceAll('-','') ;
+    const loc = getxy.filter(item => item["1단계"] === txtArea.current.value)[0] ;
+    const x = loc["격자 X"];
+    const y = loc["격자 Y"];
+
+    navigate(`/fcstlist?gubun=${gubun}&dt=${dt}&x=${x}&y=${y}&area=${txtArea.current.value}`) ;
+  }
   return (
-    <div className="w-full flex flex-col justify-center items-center">
+    <div className="w-full flex flex-col items-center">
       <h1 className="w-full text-center text-3xl my-10 font-bold">
         일기예보선택
       </h1>
       <div className="w-10/12 grid grid-cols-1 md:grid-cols-2 gap-4 place-items-center">
-        <input type='date' 
-              className="form-input w-full"
-               id = 'txt1' />
-        <select className="form-select  w-full">
-            <option value=''>--지역을 선택하세요.</option>
+        <input type='date'
+          ref = {txtDt}
+          className="form-input w-full"
+          id='txt1' />
+        <select className="form-select w-full"
+                ref={txtArea}>
+          <option value=''>--지역을 선택하세요.</option>
+          {sido}
         </select>
-        <TailButton caption = '초단기예보'
-                      color = 'blue'
-                      handleClick = ''
-                      size = 'w-1/2'/>
-        <TailButton caption = '단기예보'
-                      color = 'blue'
-                      handleClick = ''
-                      size = 'w-full'/>
+        <TailButton caption='초단기예보'
+          color='blue'
+          handleClick={() => handleOk('초단기예보')}
+          size='w-1/2' />
+        <TailButton caption='단기예보'
+          color='blue'
+          handleClick={() => handleOk('단기예보')}
+          size='w-1/2' />
       </div>
     </div>
   )
--- src/14/FcstList.js
+++ src/14/FcstList.js
@@ -1,5 +1,15 @@
-
+import { useSearchParams } from "react-router-dom" ;
 export default function FcstList() {
+  const [sParams] = useSearchParams() ;
+  //http://localhost:3000/fcstlist?gubun=%EC%B4%88%EB%8B%A8%EA%B8%B0%EC%98%88%EB%B3%B4&dt=20241015&x=98&y=76$area=%EB%B6%80%EC%82%B0%EA%B4%91%EC%97%AD%EC%8B%9C
+  const gubun = sParams.get('gubun') ;
+  const dt = sParams.get('dt') ;
+  const x = sParams.get('x') ;
+  const y = sParams.get('y') ;
+  const area = sParams.get('area') ;
+  console.log(gubun, dt, x, y, area) ;
+
+
   return (
     <div>
       일기예보항목  
