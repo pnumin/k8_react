0f0c45ff2687f2e24cb2aa1a6f309b870cd12ec0
pnumin <pnumin@pusan.ac.kr>
2024-10-16 11:59:41 +0900

    일기예보

--- src/14/FcstList.js
+++ src/14/FcstList.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { useSearchParams } from "react-router-dom" ;
 import getcode from "./getcode.json" ;
 
@@ -6,6 +6,12 @@ export default function FcstList() {
 
   //목록 
   const [ops, setOps] = useState();
+
+  //전체데이터
+  const [tdata, setTdata] = useState([]) ;
+
+  //form 값을 참조하기 위한 ref변수
+  const selRef = useRef() ;
 
   const [sParams] = useSearchParams() ;
   //http://localhost:3000/fcstlist?gubun=%EC%B4%88%EB%8B%A8%EA%B8%B0%EC%98%88%EB%B3%B4&dt=20241015&x=98&y=76$area=%EB%B6%80%EC%82%B0%EA%B4%91%EC%97%AD%EC%8B%9C
@@ -16,10 +22,47 @@ export default function FcstList() {
   const area = sParams.get('area') ;
   console.log(gubun, dt, x, y, area) ;
 
+  //select가 선택이 되었을때
+  const handleSelect = () => {
+    console.log(selRef.current.value) ;
+    if (!tdata) return ;
+
+    const tm = tdata.filter(item => item['category'] === selRef.current.value);
+
+    console.log(tm) ;
+  }
+
+  //데이터 가져오기
+  const getFetchData = async(url) => {
+    const resp = await fetch(url) ;
+    const data = await resp.json() ;
+
+    console.log(data.response.body.items.item) ;  
+    setTdata(data.response.body.items.item)  ; 
+  }
+
   //컴포넌트 생성시
   useEffect(()=>{
-    const tm = getcode.filter(item => item.예보구분 === gubun) ;
-    console.log(tm);
+    const tm = getcode.filter(item => item.예보구분 === gubun)
+                      .map(item => <option key={item.항목값}
+                                           value={item.항목값}>
+                                    {item.항목명}({item.항목값})
+                                    </option>) ;
+
+    setOps(tm);
+
+    let url ='https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0' ;
+    if (gubun === '단기예보') {
+      url = url + `/getVilageFcst?serviceKey=${process.env.REACT_APP_API_KEY }&pageNo=1&numOfRows=1000&dataType=json`;
+      url = url + `&base_date=${dt}&base_time=0500&nx=${x}&ny=${y}`;
+    }
+    else {
+      url = url + `/getUltraSrtFcst?serviceKey=${process.env.REACT_APP_API_KEY }&pageNo=1&numOfRows=1000&dataType=json`;
+      url = url + `&base_date=${dt}&base_time=0630&nx=${x}&ny=${y}`; 
+    }
+
+    console.log(url);
+    getFetchData(url) ;
   },[]);
 
   return (
@@ -29,11 +72,15 @@ export default function FcstList() {
         <h1 className="w-full text-left text-2xl font-bold">
           {area} {gubun} ({dt.slice(0,4)}.{dt.slice(4,6)}.{dt.slice(6,8)})
         </h1>
-        <select className="form-select">
+        <select className="form-select" 
+                ref={selRef}
+                onChange={handleSelect}>
           <option value=''>--항목을 선택하세요.--</option>
+          {ops}
         </select>
       </div>  
-      
+      <table>
+      </table>
     </div>
   )
 }
