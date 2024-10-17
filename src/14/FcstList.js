c270f7ab07c4a9546f426cb309291b1b4610f417
pnumin <pnumin@pusan.ac.kr>
2024-10-17 14:32:41 +0900

    일기예보

--- src/14/FcstList.js
+++ src/14/FcstList.js
@@ -16,6 +16,9 @@ export default function FcstList() {
   //form 값을 참조하기 위한 ref변수
   const selRef = useRef() ;
 
+  //sky 항목
+  const sky = {'1': '맑음(☀️)', '3':'구름많음(☁️)', '4':'흐림(🌥️)'}
+
   const [sParams] = useSearchParams() ;
   //http://localhost:3000/fcstlist?gubun=%EC%B4%88%EB%8B%A8%EA%B8%B0%EC%98%88%EB%B3%B4&dt=20241015&x=98&y=76$area=%EB%B6%80%EC%82%B0%EA%B4%91%EC%97%AD%EC%8B%9C
   const gubun = sParams.get('gubun') ;
@@ -43,7 +46,12 @@ export default function FcstList() {
                                   <td>
                                     {item.fcstTime.slice(0,2)}:{item.fcstTime.slice(2,4)}
                                   </td>
-                                  <td>{item.fcstValue}{code.단위}</td>
+                                  <td> 
+                                  {                  
+                                    item.category === 'SKY' ? sky[item.fcstValue]
+                                        : item.fcstValue + code.단위 
+                                  }
+                                  </td>
                                  </tr>);
 
     setTrs(tm) ;
