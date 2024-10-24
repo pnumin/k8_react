e997f70c17859a3a3c5281c0a0f3e4169d0642f1
pnumin <pnumin@pusan.ac.kr>
2024-10-24 12:52:05 +0900

    Restfull 처리

--- src/16/Rest.js
+++ src/16/Rest.js
@@ -4,6 +4,8 @@ export default function Rest() {
   //화면 재랜더링을 위한 state변수
   const [tdata, setTdata] = useState([]);
   const [trs, setTrs] = useState([]) ;
+  const [isUpdate, setIsUpdate] = useState(false) ; // 입력 ,수정을  확인
+  const [updateId, setUpdateId] = useState() ; // 수정할 데이터 아이디
   
   //입력값을 제어하기 위한 ref변수
   const txt1Ref = useRef();
@@ -23,6 +25,120 @@ export default function Rest() {
     setTdata(data) ;
   }
 
+  //입력처리 사용자 정의함수
+  const handlePost = async() => {
+    // 입력 확인
+    if (!txt1Ref.current.value) {
+      alert('제목을 입력하세요.') ;
+      txt1Ref.current.focus();
+      return ;
+    }
+    if (!txt2Ref.current.value) {
+      alert('작성자를 입력하세요.') ;
+      txt2Ref.current.focus();
+      return ;
+    }
+
+    //보낼 데이터 object로 만들기
+    const postData = {
+      title : txt1Ref.current.value,
+      author : txt2Ref.current.value
+    }
+
+    //post fetch
+    const resp = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(postData),
+    }) ;
+
+    //입력된 데이터 반환
+    const data = await resp.json() ;
+    console.log(data)
+
+    setTdata([data , ...tdata]) ;
+
+  }
+
+  //삭제하는 사용자 정의함수
+  const handleDelete = async(id) => {
+    console.log(id)
+
+    const resp = await fetch(`${url}/${id}`, {
+      method: 'DELETE'
+    }) ;
+
+    //삭제된 데이터 반환
+    const data = await resp.json() ;
+    console.log('delete data', data) ;
+
+    const tm = tdata.filter(item => item.id !== id) ;
+    setTdata(tm);
+  }
+
+  //수정 사용자 정의함수
+  const handleUpdate = (item) => {
+    console.log('update', item)
+    txt1Ref.current.value = item.title ;
+    txt2Ref.current.value = item.author ;
+
+    setIsUpdate(true) ;
+    setUpdateId(item.id) ;
+
+  }
+
+  //수정 처리 사용자 정의함수
+  const handlePut = async () => {
+    console.log('handlePut')
+    // 입력 확인
+    if (!txt1Ref.current.value) {
+      alert('제목을 입력하세요.') ;
+      txt1Ref.current.focus();
+      return ;
+    }
+    if (!txt2Ref.current.value) {
+      alert('작성자를 입력하세요.') ;
+      txt2Ref.current.focus();
+      return ;
+    }
+
+    //보낼 데이터 object로 만들기
+    const postData = { 
+      title : txt1Ref.current.value,
+      author : txt2Ref.current.value
+    }
+
+    //post fetch
+    const resp = await fetch(`${url}/${updateId}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(postData),
+    }) ;
+
+    //수정 데이터 반환
+    const data = await resp.json() ;
+    console.log(data)
+
+    const tm = tdata.map(item => item.id === updateId ? data : item);
+    setTdata(tm);
+
+    setIsUpdate(false) ;
+    setUpdateId('') ;
+    txt1Ref.current.value = '' ;
+    txt2Ref.current.value = '' ;
+  }
+
+  //입력과 수정을 구분하는 사용자 정의함수
+  const handleOk = () => {
+    if (!isUpdate) handlePost() ;
+    else handlePut() ;
+  }
+
+
   //컴포넌트 생성시 실행
   useEffect(()=>{
     //데이터 가져오기
@@ -35,8 +151,18 @@ export default function Rest() {
     const tm = tdata.map(item=> <tr key={item.id} className="bg-white border-b h-10 hover:bg-gray-50 cursor-pointer">
                                   <td className="text-center">{item.title}</td>
                                   <td className="text-center">{item.author}</td>
-                                  <td className="text-center">삭제</td>
-                                  <td className="text-center">수정</td>
+                                  <td className="text-center">
+                                  <TailButton caption='삭제'
+                                              color='orange'
+                                              handleClick={() => handleDelete(item.id)}
+                                              size='w-1/2' />
+                                  </td>
+                                  <td className="text-center">
+                                  <TailButton caption='수정'
+                                              color='lime'
+                                              handleClick={() => handleUpdate(item)}
+                                              size='w-1/2' />
+                                  </td>
                                 </tr>);
     setTrs(tm);
   }, [tdata]);
@@ -64,9 +190,9 @@ export default function Rest() {
             className="form-input w-full"
               />
         </div>
-        <TailButton caption='입력'
+        <TailButton caption= {isUpdate ? '수정' : '입력'}
                       color='blue'
-                      handleClick=''
+                      handleClick={handleOk}
                       size='w-1/2' /> 
       </div>
       <table
